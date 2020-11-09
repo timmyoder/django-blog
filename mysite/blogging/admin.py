35e5fcefc474a554e3582e1d8bb2b018b29a377f
Tim Yoder <tim.a.yoder@gmail.com>
2020-11-08 19:08:53 -0800

    post admin page, show categories

--- mysite/blogging/admin.py
+++ mysite/blogging/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# and a new admin registration
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategroyInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'text', 'author', 'published_date')
+    inlines = [
+        CategroyInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
