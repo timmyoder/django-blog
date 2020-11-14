4f401f161489952ef84836213266177f6f4277ae
Tim Yoder <tim.a.yoder@gmail.com>
2020-11-14 14:22:49 -0800

    blogging class based views

--- mysite/blogging/views.py
+++ mysite/blogging/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 from blogging.models import Post
 
 
@@ -15,18 +18,12 @@ def stub_view(request, *args, **kwargs):
     return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class PostListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None
+                                    ).order_by('-published_date')
+    template_name = 'blogging/list.html'
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PostDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
