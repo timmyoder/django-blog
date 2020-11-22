e9bb4b56fd86a2e38fe29d35e45eff84762c88de
Tim Yoder <tim.a.yoder@gmail.com>
2020-11-22 12:22:03 -0800

    new post model form

---
+++ blogging/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from blogging.models import Post
+
+
+class MyPostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'text', 'author']
--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blogging.views import PostListView, PostDetailView
+from blogging.views import PostListView, PostDetailView, add_model
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog_index"),
+    path("add/", add_model, name="add_post"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
 ]
