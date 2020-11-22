b441b908e19077fdd3e08124ef4eb3f46b4c36cb
rachelsmith9240 <rachelsmith9240@gmail.com>
2020-11-22 13:16:52 -0800

    Added new file serializers, made changes to urls, views, and requirements to allow for restful api.

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from blogging.views import PostListView, PostDetailView
+from rest_framework import routers
+from blogging.views import PostListView, PostDetailView, UserViewSet, CategoryViewSet, PostViewSet
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'categories', CategoryViewSet)
+
 
 urlpatterns = [
     path("", PostListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", PostDetailView.as_view(), name="blog_detail"),
+    path("api/", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
-from blogging.models import Post
+from blogging.models import Post, Category
+from django.contrib.auth.models import User
+from rest_framework import viewsets, permissions
+from blogging.serializers import UserSerializer, PostSerializer, CategorySerializer
 
 
 def stub_view(request, *args, **kwargs):
@@ -28,3 +30,30 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     queryset = Post.objects.exclude(published_date__exact=None)
     template_name = "blogging/detail.html"
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows posts to be viewed or edited.
+    """
+
+    queryset = Post.objects.all().order_by("-created_date")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+
+    queryset = Category.objects.all().order_by("-name")
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
